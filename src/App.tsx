b1270244d2c06df3767ee51614f12140c00f52d8
tada <tadataku.lab@gmail.com>
2020-03-18 01:04:41 +0900

    fet: impl mini puppy playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { PuppyVM } from '@playpuppy/puppy2d';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { PuppyEditor } from '@playpuppy/puppy-editor';
+import { ThankYouMrTada } from '@playpuppy/koinu';
+import { messagefy } from '@playpuppy/puppy2d/dist/lang/message';
+import { SourceEvent } from '@playpuppy/puppy2d/dist/lang/code';
 
 function App() {
+  const [puppy, setPuppy] = useState(null as PuppyVM | null);
+  const [editor, setEditor] = useState(null as PuppyEditor | null);
+  useEffect(() => {
+    const puppyElement = document.getElementById('puppy-screen');
+    if (puppyElement) {
+      const puppy = new PuppyVM(puppyElement);
+      setPuppy(puppy);
+    }
+    const editorElement = document.getElementById('puppy-editor');
+    if (editorElement) {
+      const editor = new PuppyEditor(editorElement, {
+        puppyCodeAction: {
+          koinuCodeAction:  (s) => {
+            return ThankYouMrTada(s)
+          }
+        },
+        messagefy: (log: any) => messagefy(log as SourceEvent)
+      });
+      editor.setModel("print('Hello')", "python");
+      setEditor(editor);
+    }
+  }, []);
+  
+  useEffect(() => {
+    if(puppy && editor) {
+      puppy.addEventListener('error', editor.addSourceEvent.bind(editor));
+      puppy.addEventListener('warning', editor.addSourceEvent.bind(editor));
+      puppy.addEventListener('info', editor.addSourceEvent.bind(editor));
+      editor.callback = (source: string) => puppy.load(source, false)
+    }
+  }, [puppy, editor])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container style={{maxWidth: "100vw", height: "100vh", margin: "0", padding: "0"}}>
+        <Row style={{width: "100%", height: "100%"}}>
+          <Col>
+            <div
+              id="puppy-screen"
+              style={{width: "100%", height: "100%"}}
+            >
+            </div>
+            <div style={{width: "100%", textAlign: "right", position: "absolute", bottom: "12px", right: "20px"}}>
+              <Button
+                onClick={() => puppy ? puppy.load(editor?.editor.getValue(), true) : false}
+              >
+                Play
+              </Button>
+            </div>
+          </Col>
+          <Col>
+            <div
+              id="puppy-editor"
+              style={{width: "100%", height: "100%", textAlign: "left"}}
+            >
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
